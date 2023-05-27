385e19927cac7668ba0c2e95ab8ed68f44aea301
Conan <Conan@qq.com>
2023-05-27 18:44:12 +0800

    first commit

---
+++ src/main/java/com/conan/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.conan.git;
+
+/**
+ * @Author Conan
+ * @Description TODO
+ * @Date 2023/5/27 18:42
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git test");
+    }
+}
